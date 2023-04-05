1f2aaa9ab12b178e0b70f4fd7b5f5875d17ae65b
YUKI JUNIOR <ukijunioruesugi@gmail.com>
2023-04-05 20:36:32 -0300

    conversoes

---
+++ conversoes.js
@@ -0,0 +1,27 @@
+// Tipo de Dados
+
+
+
+//boolean
+
+// Conversão Explicitica
+
+const numero = 456;
+const numeroString = Number ("456");
+
+/* Para Fazer as conversões vc precisa usar 
+String e Number segue exemplo:
+Fazendo a conversão: Number
+comando abaixo convertendo no console.
+console.log(numero + Number(numeroString));
+Por semantica e para evitar bug o melhor é 
+fazemos direto na variável 
+*/
+console.log(numero + numeroString); 
+
+
+
+// Conversão Implicita
+
+
+
